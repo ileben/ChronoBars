21d835e2d5840ea2ea480506f150e5d1cb679581
Ivan Leben <ivan.leben@gmail.com>
2011-04-09 22:45:36 +1000

    Constructing the whole config menu...

--- ChronoBars_GlobalOptions.lua
+++ ChronoBars_GlobalOptions.lua
@@ -14,53 +14,208 @@ local CB = ChronoBars;
 --===========================================================================
 
 
-ChronoBars.Frame_Root =
+ChronoBars.Frame_Root = 
 {
-	{ type="tabs", tabs="root|Tabs_Root" },
+	{ type="tabs",     tabs="root|Tabs_Root" },
 };
 
 ChronoBars.Tabs_Root =
 {
-	{ text="Hello",   frame="root|Frame_Test" },
-	{ text="Kitty",   frame="root|Frame_Test2" },
-	{ text="Cat" },
+	{ text="Bar",      frame="root|Frame_Bar" },
+	{ text="Group",    frame="root|Frame_Group" },
+	{ text="Profile",  frame="root|Frame_Profile" },
 };
 
-ChronoBars.Frame_Test =
+--Bar
+
+ChronoBars.Frame_Bar =
 {
-	{ type="options",   text="Options:",       options="root|Options_Test" },
-	{ type="input",     text="Input:" },
-	{ type="toggle",    text="Enabled" },
-	{ type="input",     text="Other input:" },
-	{ type="tabs",      tabs="root|Tabs_Test" },
+	{ type="header",   text="bar|name" },
+	{ type="toggle",   text="Enabled",          var="bar|enabled" },
+	{ type="tabs",     tabs="root|Tabs_Bar" },
 };
 
-ChronoBars.Options_Test =
+ChronoBars.Tabs_Bar =
 {
-	{ text="Test1" },
-	{ text="Test2" },
-	{ text="Test3" },
+	{ text="Effect",      frame="root|Frame_Effect" },
+	{ text="Appearance",  frame="root|Frame_Appearance" },
+	{ text="Copy",        frame="root|Frame_Copy" },
+	{ text="Paste",       frame="root|Frame_Paste" },
 };
 
-ChronoBars.Tabs_Test =
+ChronoBars.Frame_Effect =
 {
-	{ text="Hello" },
-	{ text="World" },
-	{ text="of Warcraft" },
+	{ type="input",       text="Effect name",     var="bar|name" },
+	{ type="options",     text="Effect type",     var="bar|type",   options="root|Options_EffectType" },
+	
+	{ type="group",       text="Aura Settings",           frame="root|Frame_AuraSettings",
+	  conditionVar="bar|type", conditionValue = ChronoBars.EFFECT_TYPE_AURA },
+	  
+	{ type="group",       text="Multi-Aura Settings",     frame="root|Frame_MultiAuraSettings",
+	  conditionVar="bar|type", conditionValue = ChronoBars.EFFECT_TYPE_MULTI_AURA },
+	  
+	{ type="group",       text="Cooldown settings", frame="root|Frame_CooldownSettings",
+	  conditionVar="bar|type", conditionValue = ChronoBars.EFFECT_TYPE_CD },
+
+	{ type="group",       text="Usable settings",   frame="root|Frame_UsableSettings",
+	conditionVar="bar|type", conditionValue = ChronoBars.EFFECT_TYPE_USABLE },
+
+	{ type="group",       text="Totem settings",    frame="root|Frame_TotemSettings",
+	  conditionVar="bar|type", conditionValue = ChronoBars.EFFECT_TYPE_TOTEM },
+
+	{ type="group",       text="Custom settings",   frame="root|Frame_CustomSettings",
+	  conditionVar="bar|type", conditionValue = ChronoBars.EFFECT_TYPE_CUSTOM },
+
+	{ type="group",       text="Auto-Attack settings", frame="root|Frame_AutoSettings",
+	  conditionVar="bar|type", conditionValue = ChronoBars.EFFECT_TYPE_AUTO },
+
+	{ type="group",       text="Enchant settings",     frame="root|Frame_EnchantSettings",
+	  conditionVar="bar|type", conditionValue = ChronoBars.EFFECT_TYPE_ENCHANT },
 };
 
-ChronoBars.Frame_Test2 =
+ChronoBars.Options_EffectType =
 {
-	{ type="input",      text="Some other input:" },
-	{ type="options",    text="Some other options:",      options="root|Options_Test2" },
+  { text="Aura",               value = ChronoBars.EFFECT_TYPE_AURA },
+  { text="Multi Aura",         value = ChronoBars.EFFECT_TYPE_MULTI_AURA },
+  { text="Cooldown",           value = ChronoBars.EFFECT_TYPE_CD },
+  { text="Usable",             value = ChronoBars.EFFECT_TYPE_USABLE },
+  { text="Totem",              value = ChronoBars.EFFECT_TYPE_TOTEM },
+  { text="Custom",             value = ChronoBars.EFFECT_TYPE_CUSTOM },
+  { text="Auto-Attack",        value = ChronoBars.EFFECT_TYPE_AUTO },
+  { text="Weapon Enchant",     value = ChronoBars.EFFECT_TYPE_ENCHANT },
 };
 
-ChronoBars.Options_Test2 =
+ChronoBars.Frame_AuraSettings =
 {
-	{ text="Test1" },
-	{ text="Test2" },
-	{ text="Test3" },
+	{ type="options",   text="Aura type",                   var="bar|aura.type",   options="root|Options_AuraType" },
+	{ type="options",   text="Unit to monitor",             var="bar|aura.unit",   options="root|Options_AuraUnit" },
+	{ type="options",   text="Which when multiple",         var="bar|aura.order",  options="root|Options_AuraOrder" },
+	{ type="toggle",    text="Only if cast by self",        var="bar|aura.byPlayer" },
+	{ type="toggle",    text="Sum stack from all casters",  var="bar|aura.sum" },
 };
+
+ChronoBars.Options_AuraType =
+{
+  { text="Buff",   value = ChronoBars.AURA_TYPE_BUFF },
+  { text="Debuff", value = ChronoBars.AURA_TYPE_DEBUFF },
+};
+
+ChronoBars.Options_AuraUnit =
+{
+  { text="Player",             value = ChronoBars.AURA_UNIT_PLAYER },
+  { text="Target",             value = ChronoBars.AURA_UNIT_TARGET },
+  { text="Focus",              value = ChronoBars.AURA_UNIT_FOCUS },
+  { text="Pet",                value = ChronoBars.AURA_UNIT_PET },
+  { text="Target of target",   value = ChronoBars.AURA_UNIT_TARGET_TARGET },
+  { text="Target of focus",    value = ChronoBars.AURA_UNIT_FOCUS_TARGET },
+  { text="Target of pet",      value = ChronoBars.AURA_UNIT_PET_TARGET },
+};
+
+ChronoBars.Options_AuraOrder =
+{
+  { text="First",    value = 1 },
+  { text="Second",   value = 2 },
+  { text="Third",    value = 3 },
+  { text="Fourth",   value = 4 },
+  { text="Fifth",    value = 5 },
+};
+
+ChronoBars.Frame_MultiAuraSettings =
+{
+	{ type="options",   text="Aura type",                     var="bar|aura.type",       options="root|Options_AuraType" },
+	{ type="numinput",  text="Estimate duration (seconds)",   var="bar|custom.duration" },
+	{ type="toggle",    text="Only if cast by self",          var="bar|aura.byPlayer" },
+};
+
+
+ChronoBars.Frame_CooldownSettings =
+{
+	{ type="options",   text="Cooldown type",    var="bar|cd.type",    options="root|Options_CooldownType" },
+};
+
+ChronoBars.Options_CooldownType =
+{
+  { text="Spell",       value = ChronoBars.CD_TYPE_SPELL },
+  --{ text="Pet Spell",   value = ChronoBars.CD_TYPE_PET_SPELL },
+  { text="Item",        value = ChronoBars.CD_TYPE_ITEM },
+};
+
+ChronoBars.Frame_UsableSettings =
+{
+};
+
+ChronoBars.Frame_TotemSettings =
+{
+};
+
+ChronoBars.Frame_CustomSettings =
+{
+};
+
+ChronoBars.Frame_AutoSettings =
+{
+};
+
+ChronoBars.Frame_EnchantSettings =
+{
+};
+
+ChronoBars.Frame_Appearance =
+{  
+	{ type="tabs",    tabs="root|Tabs_Appearance" },
+};
+
+ChronoBars.Tabs_Appearance =
+{
+	{ text="Bar",        frame="root|Frame_StyleBar" },
+	{ text="Icon" },
+	{ text="Text" },
+	{ text="Animation" },
+	{ text="Visibility" },
+};
+
+ChronoBars.Frame_StyleBar =
+{	
+	{ type="options",    text="Direction",     var="bar|style.fullSide",   options="root|Options_FullSide" },
+	{ type="toggle",     text="Fill up",       var="bar|style.fillUp" },
+
+	--{ type="separator" },
+	
+	{ type="options",    text="Texture",       var="bar|style.lsmTexHandle",  options="root|Options_Texture"	},
+	{ type="toggle",     text="Front Color"  }, --, var="bar|style.fgColor" },
+	{ type="toggle",     text="Back Color"  },  --, var="bar|style.bgColor" },
+};
+
+ChronoBars.Options_FullSide =
+{
+	{ text="Empty || Full",  value = ChronoBars.SIDE_RIGHT },
+	{ text="Full || Empty",  value = ChronoBars.SIDE_LEFT },
+};
+
+ChronoBars.Options_Texture =
+{
+};
+
+ChronoBars.Frame_Copy =
+{
+};
+
+ChronoBars.Frame_Past =
+{
+};
+
+--Group
+
+ChronoBars.Frame_Group =
+{
+};
+
+--Profile
+
+ChronoBars.Frame_Profile =
+{
+};
+
 
 --Construction
 --===========================================================================
@@ -91,14 +246,22 @@ function ChronoBars.Config_Construct( container, config )
 	for i = 1, numItems do
 	repeat
 
-		--Construct item based on type
 		local item = config[i];
-		local frame = nil;
+
+		--Check for condition		
+		if (item.conditionVar ~= nil) then
+			local curValue = CB.GetSettingsValue( CB.MenuId, item.conditionVar );
+			if (curValue ~= item.conditionValue) then
+				break;
+			end
+		end
 		
+		--Construct item based on type
+		local frame = nil;
 		
 		if (item.type == "tabs") then
 		
-			--Create tab object
+			--Create tab frame object
 			local tabFrame = CB.NewObject( "tabframe" );
 			tabFrame:SetPoint( "BOTTOMRIGHT", container, "BOTTOMRIGHT" );
 			tabFrame.OnTabChanged = ChronoBars.Config_OnTabChanged;
@@ -129,13 +292,36 @@ function ChronoBars.Config_Construct( container, config )
 				CB.Config_Construct( tabFrame.container, frameConfig );
 			end
 			
+		elseif (item.type == "group") then
+		
+			--Create group frame object
+			local grpFrame = CB.NewObject( "groupframe" );
+			--grpFrame:SetHeight( 100 );
+			grpFrame:SetLabelText( item.text );
+			frame = grpFrame;
+			
+			--Get frame config
+			local frameConfig = CB.GetSettingsValue( nil, item.frame );
+			if (frameConfig ~= nil) then
+			
+				--Construct sub frame
+				CB.Config_Construct( grpFrame.container, frameConfig );
+			end
+			
+		elseif (item.type == "header") then
+		
+			--Create header frame object
+			local hdrFrame = CB.NewObject( "header" );
+			hdrFrame:SetText( CB.GetSettingsValue( CB.MenuId, item.text ));
+			frame = hdrFrame;
 			
 		elseif (item.type == "options") then
 			
 			--Create dropdown object
 			local ddFrame = CB.NewObject( "dropdown" );
 			ddFrame:SetLabelText( item.text );
-			ddFrame.OnOptionChanged = ChronoBars.Config_OnOptionChanged;
+			ddFrame.OnValueChanged = ChronoBars.Config_OnOptionChanged;
+			ddFrame.item = item;
 			frame = ddFrame;
 			
 			--Get options config
@@ -147,28 +333,45 @@ function ChronoBars.Config_Construct( container, config )
 			
 			--Add items from config
 			for d = 1, table.getn(ddConfig) do
-				ddFrame:AddItem( ddConfig[d].text, item );
+				ddFrame:AddItem( ddConfig[d].text, ddConfig[d].value );
 			end
+			
+			--Get value and apply to object
+			local curValue = ChronoBars.GetSettingsValue( CB.MenuId, item.var );
+			ddFrame:SelectValue( curValue );
+			
 			
 		elseif (item.type == "toggle") then
 		
 			--Create checkbox object
 			local cbFrame = CB.NewObject( "checkbox" );
 			cbFrame:SetText( item.text );
+			cbFrame.OnValueChanged = ChronoBars.Config_OnToggleChanged;
+			cbFrame.item = item;
 			frame = cbFrame;
 			
-		elseif (item.type == "input") then
+			--Get value and apply to object
+			local curValue = ChronoBars.GetSettingsValue( CB.MenuId, item.var );
+			cbFrame:SetChecked( curValue );
+			
+		elseif (item.type == "input" or item.type == "numinput") then
 		
 			--Create input object
 			local inpFrame = CB.NewObject( "input" );
 			inpFrame:SetLabelText( item.text );
+			inpFrame.OnValueChanged = ChronoBars.Config_OnInputChanged;
+			inpFrame.item = item;
 			frame = inpFrame;
+			
+			--Get value and apply to object
+			local curValue = ChronoBars.GetSettingsValue( CB.MenuId, item.var );
+			inpFrame:SetText( tostring(curValue) );
+			
 		end
 		
-		--Position item below previous one			
+		--Position item below previous one
 		if (prevFrame == nil)
 		then frame:SetPoint( "TOPLEFT", container, "TOPLEFT", 0,0 );
-		--else frame:SetPoint( "TOPLEFT", prevFrame, "BOTTOMLEFT", 0,0 );
 		else frame:SetPoint( "TOPLEFT", prevFrame, "BOTTOMLEFT", 0,-10 );
 		end
 		
@@ -197,7 +400,26 @@ function ChronoBars.Config_OnOptionChanged( ddFrame )
 
 	CB.Print( "OPTION CHANGED" );
 	
-	local item = ddFrame:GetSelectedValue();
+	local item = ddFrame.item;
+	local newValue = ddFrame:GetSelectedValue();
+	
+end
+
+function ChronoBars.Config_OnToggleChanged( cbFrame )
+
+	CB.Print( "TOGGLE CHANGED" );
+	
+	local item = cbFrame.item;
+	local newValue = cbFrame:GetChecked();
+	
+end
+
+function ChronoBars.Config_OnInputChanged( inpFrame )
+
+	CB.Print( "INPUT CHANGED" );
+	
+	local item = inpFrame.item;
+	local newValue = inpFrame:GetText();
 	
 end
 
@@ -219,7 +441,7 @@ function ChronoBars.OpenBarConfig (bar)
 
 	if (not CB.configFrame) then
 	
-		CB.configFrame = CB.Frame_New( "ChronoBars.ConfigFrame", "Config", true);
+		CB.configFrame = CB.Frame_New( "ChronoBars.ConfigFrame", "ChronoBars", true);
 		
 	end
 	
--- ChronoBars_OptionsGui.lua
+++ ChronoBars_OptionsGui.lua
@@ -33,10 +33,14 @@ function ChronoBars.Input_New( name )
 	i:SetPoint( "TOPRIGHT", l, "BOTTOMRIGHT", -1,0 );
 	i:SetHeight( 30 );
 	i:SetAutoFocus( false );
+	i:SetScript( "OnEnterPressed", ChronoBars.Input_OnEnterPressed );
+	i.frame = f;
 	f.input = i;
 	
 	--Functions
 	f.SetLabelText = ChronoBars.Input_SetLabelText;
+	f.SetText      = ChronoBars.Input_SetText;
+	f.GetText      = ChronoBars.Input_GetText;
 	return f;
 end
 
@@ -52,6 +56,12 @@ function ChronoBars.Input_GetText( frame )
 	return frame.input:GetText();
 end
 
+function ChronoBars.Input_OnEnterPressed( input )
+	local frame = input.frame;
+	local script = frame.OnValueChanged;
+	if (script) then script( frame ); end
+end
+
 --Check box
 --====================================================================
 
@@ -64,6 +74,9 @@ function ChronoBars.Checkbox_New( name )
 	--This is the actual checkbox
 	local c = CreateFrame( "CheckButton", name, f, "UICheckButtonTemplate" );
 	c:SetPoint( "TOPLEFT", f, "TOPLEFT", -5, 5 );
+	c:SetScript( "OnClick", ChronoBars.Checkbox_OnClick );
+	c.frame = f;
+	f.check = c;
 	
 	--Get font string that contains the text
 	f.text = _G[ name.."Text" ];
@@ -71,13 +84,31 @@ function ChronoBars.Checkbox_New( name )
 	f.text:SetTextColor( 1,1,1 );
 	
 	--Functions
-	f.SetText = ChronoBars.Checkbox_SetText;
+	f.SetText     = ChronoBars.Checkbox_SetText;
+	f.SetChecked  = ChronoBars.Checkbox_SetChecked;
+	f.GetChecked  = ChronoBars.Checkbox_GetChecked;
 	return f;
 end
 
 function ChronoBars.Checkbox_SetText( frame, text )
 	frame.text:SetText( text );
 end
+
+function ChronoBars.Checkbox_SetChecked( frame, checked )
+	frame.check:SetChecked( checked );
+end
+
+function ChronoBars.Checkbox_GetChecked( frame )
+	return frame.check:GetChecked();
+end
+
+function ChronoBars.Checkbox_OnClick( check )
+	local frame = check.frame;
+	local script = frame.OnValueChanged;
+	if (script) then script( frame ); end
+end
+
+
 
 --Dropdown box
 --=====================================================================
@@ -109,7 +140,8 @@ function ChronoBars.Dropdown_New( name )
 	f.values = {};
 	f.Free             = ChronoBars.Dropdown_Free;
 	f.AddItem          = ChronoBars.Dropdown_AddItem;
-	f.SelectItem       = ChronoBars.Dropdown_SelectItem;
+	f.SelectIndex      = ChronoBars.Dropdown_SelectIndex;
+	f.SelectValue      = ChronoBars.Dropdown_SelectValue;
 	f.GetSelectedIndex = ChronoBars.Dropdown_GetSelectedIndex;
 	f.GetSelectedText  = ChronoBars.Dropdown_GetSelectedText;
 	f.GetSelectedValue = ChronoBars.Dropdown_GetSelectedValue;
@@ -146,25 +178,33 @@ function ChronoBars.Dropdown_Initialize( frame )
 end
 
 function ChronoBars.Dropdown_SetBoxWidth( frame, width )
-  UIDropDownMenu_SetWidth( frame.drop, width );
+	UIDropDownMenu_SetWidth( frame.drop, width );
 end
 
 function ChronoBars.Dropdown_AddItem( frame, text, value )
-  table.insert( frame.items, text );
-  frame.values[ table.getn(frame.items) ] = value;
-  UIDropDownMenu_Initialize( frame.drop, function () frame:Initialize() end );
+	table.insert( frame.items, text );
+	frame.values[ table.getn(frame.items) ] = value;
+	UIDropDownMenu_Initialize( frame.drop, function () frame:Initialize() end );
 end
 
-function ChronoBars.Dropdown_SelectItem( frame, index )
-  UIDropDownMenu_SetSelectedValue( frame.drop, index );
+function ChronoBars.Dropdown_SelectIndex( frame, index )
+	UIDropDownMenu_SetSelectedValue( frame.drop, index );
+end
+
+function ChronoBars.Dropdown_SelectValue( frame, value )
+	for i,v in pairs( frame.values ) do
+		if (v == value) then
+			UIDropDownMenu_SetSelectedValue( frame.drop, i );
+		end
+	end
 end
 
 function ChronoBars.Dropdown_GetSelectedIndex( frame )
-  return UIDropDownMenu_GetSelectedValue( frame.drop );
+	return UIDropDownMenu_GetSelectedValue( frame.drop );
 end
 
 function ChronoBars.Dropdown_GetSelectedText( frame )
-  return UIDropDownMenu_GetText( frame.drop );
+	return UIDropDownMenu_GetText( frame.drop );
 end
 
 function ChronoBars.Dropdown_GetSelectedValue( frame )
@@ -183,11 +223,122 @@ function ChronoBars.DropdownItem_Func( item )
 	UIDropDownMenu_SetSelectedValue( frame.drop, item.value );
 	
 	--Execute script if registered
-	local script = frame.OnOptionChanged;
+	local script = frame.OnValueChanged;
 	if (script) then script( frame ); end
 end
 
 function ChronoBars.Dropdown_SetLabelText( frame, text )
+	frame.label:SetText( text );
+end
+
+--Header
+--=====================================================================
+
+function ChronoBars.Header_New( name )
+
+	--Frame
+	local f = CreateFrame( "Frame", name, nil );
+	f:SetHeight( 25 );
+	
+	--Background
+	local b = f:CreateTexture( nil, "ARTWORK" );
+	b:SetTexture( "Interface\\AddOns\\ChronoBars\\Textures\\HeaderCentered.tga" );
+	b:SetAllPoints( f );
+	
+	--Label
+	local l = f:CreateFontString( name.."Label", "OVERLAY", "GameFontNormal" );
+	l:SetFont( "Fonts\\FRIZQT__.TTF", 12 );
+	l:SetAllPoints( f );
+	l:SetText( "Group" );
+	f.label = l;
+	
+	--Functions
+	f.SetText = ChronoBars.Header_SetText;
+	
+	return f;
+end
+
+function ChronoBars.Header_SetText( frame, text )
+	frame.label:SetText( text );
+end
+
+--Group Frame
+--=====================================================================
+
+function ChronoBars.GroupFrame_New( name )
+
+	--[[
+	--Frame
+	local f = CreateFrame( "Frame", name, nil );
+	
+	--Label
+	local l = f:CreateFontString( name.."Label", "OVERLAY", "GameFontNormal" );
+	l:SetFont( "Fonts\\FRIZQT__.TTF", 12 );
+	l:SetJustifyH( "LEFT" );
+	l:SetPoint( "TOPLEFT", 0,0 );
+	l:SetPoint( "TOPRIGHT", 0,0 );
+	l:SetHeight( 12 );
+	f.label = l;
+	
+	--Border
+	local b = CreateFrame( "Frame", name.."Border", f );
+	
+	b:SetBackdrop(
+		{bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
+		 edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+		 tile = true, tileSize = 16, edgeSize = 16,
+		 insets = { left = 3, right = 3, top = 5, bottom = 3 }});
+	
+	b:SetBackdropColor( 0.1, 0.1, 0.1, 0.5 );
+	b:SetBackdropBorderColor( 1.0, 0.9, 0.0 );
+	b:SetPoint( "TOPLEFT", 0, -17 );
+	b:SetPoint( "BOTTOMRIGHT", 0, 0 );
+	
+	--Container
+	local pad = 15;
+	local c = CreateFrame( "Frame", name.."Container", b );
+	c:SetPoint( "BOTTOMLEFT", pad, pad );
+	c:SetPoint( "TOPRIGHT", -pad,-pad );
+	f.container = c;
+	
+	--Functions
+	f.SetLabelText = ChronoBars.GroupFrame_SetLabelText;
+	
+	return f;
+	--]]
+	
+	--Frame
+	local f = CreateFrame( "Frame", name, nil );
+	f:SetHeight( 100 );
+	
+	--Header
+	local h = f:CreateTexture( nil, "ARTWORK" );
+	h:SetTexture( "Interface\\AddOns\\ChronoBars\\Textures\\HeaderCentered.tga" );
+	h:SetPoint( "TOPLEFT", 0,0 );
+	h:SetPoint( "TOPRIGHT", 0,0 );
+	h:SetHeight( 25 );
+	
+	--Label
+	local l = f:CreateFontString( name.."Label", "OVERLAY", "GameFontNormal" );
+	l:SetFont( "Fonts\\FRIZQT__.TTF", 12 );
+	l:SetAllPoints( h );
+	l:SetText( "Group" );
+	f.label = l;
+	
+	--Container
+	local c = CreateFrame( "Frame", name.."Container", f );
+	c:SetPoint( "TOPLEFT", h, "BOTTOMLEFT", 0, -10 );
+	c:SetPoint( "BOTTOMRIGHT", f, "BOTTOMRIGHT", 0, 0 );
+	f.container = c;
+	
+	--Functions
+	f.SetLabelText = ChronoBars.GroupFrame_SetLabelText;
+	
+	return f;
+end
+
+
+function ChronoBars.GroupFrame_SetLabelText( frame, text )
 	frame.label:SetText( text );
 end
 
@@ -246,7 +397,7 @@ function ChronoBars.TabFrame_New( name )
 	f:SetScript( "OnSizeChanged", ChronoBars.TabFrame_OnSizeChanged );
 	
 	--Border
-	local b = CreateFrame( "Frame", "ChronoBars.TabFrameBorder", f );
+	local b = CreateFrame( "Frame", name.."Border", f );
 	
 	b:SetBackdrop(
 		{bgFile = "Interface\\ChatFrame\\ChatFrameBackground",
@@ -265,7 +416,7 @@ function ChronoBars.TabFrame_New( name )
 	c:SetPoint( "TOPRIGHT", -pad,-pad );
 	f.container = c;
 	
-	--Tabs
+	--Private vars
 	f.tabs = {};
 	f.titles = {};
 	f.widths = {};
@@ -274,6 +425,7 @@ function ChronoBars.TabFrame_New( name )
 	f.border = b;
 	f.selected = 0;
 	
+	--Functions
 	f.Free              = ChronoBars.TabFrame_Free;
 	f.AddTab            = ChronoBars.TabFrame_AddTab;
 	f.SelectTab         = ChronoBars.TabFrame_SelectTab;
@@ -589,11 +741,13 @@ function ChronoBars.NewObject( class )
 		--Create new object if capacity exhausted
 		factory.used = capacity + 1;
 
-		if     (class == "input")    then object = CB.Input_New    ( "ChronoBars.Input"    .. factory.used );
-		elseif (class == "checkbox") then object = CB.Checkbox_New ( "ChronoBars.Checkbox" .. factory.used );
-		elseif (class == "dropdown") then object = CB.Dropdown_New ( "ChronoBars.Dropdown" .. factory.used );
-		elseif (class == "tab")      then object = CB.Tab_New      ( "ChronoBars.Tab"      .. factory.used );
-		elseif (class == "tabframe") then object = CB.TabFrame_New ( "ChronoBars.TabFrame" .. factory.used );
+		if     (class == "input")      then object = CB.Input_New      ( "ChronoBars.Input"      .. factory.used );
+		elseif (class == "checkbox")   then object = CB.Checkbox_New   ( "ChronoBars.Checkbox"   .. factory.used );
+		elseif (class == "dropdown")   then object = CB.Dropdown_New   ( "ChronoBars.Dropdown"   .. factory.used );
+		elseif (class == "header")     then object = CB.Header_New     ( "ChronoBars.Header"     .. factory.used );
+		elseif (class == "groupframe") then object = CB.GroupFrame_New ( "ChronoBars.GroupFrame" .. factory.used );
+		elseif (class == "tab")        then object = CB.Tab_New        ( "ChronoBars.Tab"        .. factory.used );
+		elseif (class == "tabframe")   then object = CB.TabFrame_New   ( "ChronoBars.TabFrame"   .. factory.used );
 		end
 
 		object.factoryClass = class;
