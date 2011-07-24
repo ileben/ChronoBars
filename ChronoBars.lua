80dc40b1fac26ce41e9c55e438b31e170e93c4d2
Ivan Leben <ivan.leben@gmail.com>
2011-07-24 23:23:34 +1000

    Bump version to 2.0

--- ChronoBars.lua
+++ ChronoBars.lua
@@ -13,7 +13,7 @@ local CB = ChronoBars;
 --Version control
 --=================================================================================
 
-ChronoBars.VERSION = "1.13";
+ChronoBars.VERSION = "2.0";
 ChronoBars.UPGRADE_LIST = { "1.2","1.3","1.4","1.5","1.6","1.8", "1.9", "1.10", "1.12", "2.0" };
 
 function ChronoBars.CompareVersions (old, new)
